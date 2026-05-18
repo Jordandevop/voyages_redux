7621f7c8df8f6f61f6b1b3b0cb79b01c914e9340
Jordan <jordan.valente@hotmail.fr>
2026-05-18 10:48:55 +0200

    add destinations

---
+++ src/data/destinations.js
@@ -0,0 +1,167 @@
+export const destinations = [
+    {
+        id: 1,
+        slug: "japan",
+        name: "Japon",
+        region: "Asie",
+        capital: "Tokyo",
+        description:
+            "Le Japon melange traditions anciennes, temples, megalopoles modernes et paysages naturels spectaculaires.",
+        image:
+            "https://cdn.pixabay.com/photo/2020/01/31/20/03/japan-4808857_1280.jpg",
+    },
+    {
+        id: 2,
+        slug: "iceland",
+        name: "Islande",
+        region: "Europe",
+        capital: "Reykjavik",
+        description:
+            "L'Islande est connue pour ses volcans, glaciers, cascades, sources chaudes et paysages lunaires.",
+        image:
+            "https://cdn.pixabay.com/photo/2019/11/24/14/06/iceland-4649477_1280.jpg",
+    },
+    {
+        id: 3,
+        slug: "morocco",
+        name: "Maroc",
+        region: "Afrique",
+        capital: "Rabat",
+        description:
+            "Le Maroc offre des medinas, des montagnes, le desert, une cuisine riche et une forte identite culturelle.",
+        image:
+            "https://cdn.pixabay.com/photo/2017/05/27/22/33/morocco-2349647_1280.jpg",
+    },
+    {
+        id: 4,
+        slug: "canada",
+        name: "Canada",
+        region: "Amerique",
+        capital: "Ottawa",
+        description:
+            "Le Canada propose de grands espaces naturels, des lacs, des forets, des montagnes et des villes multiculturelles.",
+        image:
+            "https://cdn.pixabay.com/photo/2019/08/01/17/23/ottawa-4377995_1280.jpg",
+    },
+    {
+        id: 5,
+        slug: "Sweden",
+        name: "Suède",
+        region: "Europe",
+        capital: "Stockholm",
+        description:
+            "La Suède est un pays scandinave composé de milliers d'îles côtières et lacs intérieurs, ainsi que de vastes forêts boréales et de montagnes glaciaires.",
+        image:
+            "https://cdn.pixabay.com/photo/2016/12/29/13/25/lappland-sweden-1938531_1280.jpg",
+    },
+    {
+        id: 6,
+        slug: "peru",
+        name: "Pérou",
+        region: "Amérique",
+        capital: "Lima",
+        description:
+            "Le Pérou abrite une partie de la forêt amazonienne et le Machu Picchu, une ancienne cité inca perchée dans les Andes.",
+        image:
+            "https://cdn.pixabay.com/photo/2016/01/13/17/48/machupicchu-1138641_1280.jpg",
+    },
+    {
+        id: 7,
+        slug: "new-zealand",
+        name: "Nouvelle-Zélande",
+        region: "Océanie",
+        capital: "Wellington",
+        description:
+            "La Nouvelle-Zélande se distingue par ses volcans, ses fjords spectaculaires, ses plages de sable fin et sa culture maorie.",
+        image:
+            "https://cdn.pixabay.com/photo/2016/01/01/18/24/wellington-1116880_1280.jpg",
+    },
+    {
+        id: 8,
+        slug: "italy",
+        name: "Italie",
+        region: "Europe",
+        capital: "Rome",
+        description:
+            "L'Italie est réputée pour sa cuisine mondiale, ses chefs-d'œuvre artistiques, son histoire romaine et ses côtes méditerranéennes.",
+        image:
+            "https://cdn.pixabay.com/photo/2014/08/20/22/27/colosseum-422942_1280.jpg",
+    },
+    {
+        id: 9,
+        slug: "madagascar",
+        name: "Madagascar",
+        region: "Afrique",
+        capital: "Antananarivo",
+        description:
+            "Madagascar est une immense nation insulaire célèbre pour ses baobabs, ses lémuriens et sa biodiversité unique au monde.",
+        image:
+            "https://cdn.pixabay.com/photo/2019/11/04/14/37/madagascar-4601292_1280.jpg",
+    },
+    {
+        id: 10,
+        slug: "costa-rica",
+        name: "Costa Rica",
+        region: "Amérique",
+        capital: "San José",
+        description:
+            "Le Costa Rica est un pays d'Amérique centrale réputé pour ses plages, ses volcans, ses forêts tropicales et sa philosophie de vie 'Pura Vida'.",
+        image:
+            "https://cdn.pixabay.com/photo/2015/04/11/22/15/volcano-718277_1280.jpg",
+    },
+    {
+        id: 11,
+        slug: "portugal",
+        name: "Portugal",
+        region: "Europe",
+        capital: "Lisbonne",
+        description:
+            "Le Portugal séduit par ses plages dorées de l'Algarve, ses châteaux médiévaux, sa musique Fado et son architecture recouverte d'azulejos.",
+        image:
+            "https://cdn.pixabay.com/photo/2021/05/30/16/00/port-6296267_1280.jpg",
+    },
+    {
+        id: 12,
+        slug: "australia",
+        name: "Australie",
+        region: "Océanie",
+        capital: "Canberra",
+        description:
+            "L'Australie offre des plages de surf mythiques, la Grande Barrière de corail, l'Outback sauvage et une faune unique au monde.",
+        image:
+            "https://cdn.pixabay.com/photo/2018/11/05/11/46/australia-3795891_1280.jpg",
+    },
+    {
+        id: 13,
+        slug: "egypt",
+        name: "Égypte",
+        region: "Afrique",
+        capital: "Le Caire",
+        description:
+            "L'Égypte fascine par ses millénaires d'histoire, ses pyramides majestueuses, ses temples pharaoniques le long du Nil et ses récifs de la mer Rouge.",
+        image:
+            "https://cdn.pixabay.com/photo/2017/08/13/17/48/egypt-tour-packages-2637992_1280.jpg",
+    },
+    {
+        id: 14,
+        slug: "brazil",
+        name: "Brésil",
+        region: "Amérique",
+        capital: "Brasília",
+        description:
+            "Le Brésil est célèbre pour son carnaval vibrant, la plage de Copacabana, la statue du Christ Rédempteur et l'immensité de l'Amazonie.",
+        image:
+            "https://cdn.pixabay.com/photo/2016/07/22/04/03/rio-de-janeiro-1534128_1280.jpg",
+    },
+    {
+        id: 15,
+        slug: "thailand",
+        name: "Thaïlande",
+        region: "Asie",
+        capital: "Bangkok",
+        description:
+            "La Thaïlande est réputée pour ses plages tropicales, ses palais royaux opulents, ses ruines anciennes et ses temples ornés de figures de Bouddha.",
+        image:
+            "https://cdn.pixabay.com/photo/2023/05/16/09/15/bangkok-7997046_1280.jpg",
+    },
+];
