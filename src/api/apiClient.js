3a188ccc7083faf995322c25c20c1f6a2f366f5f
Jordan <jordan.valente@hotmail.fr>
2026-05-19 17:03:43 +0200

    add headers

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -14,7 +14,31 @@ export async function apiReequest(endpoint, options = {}) {
     }
 
     if(token) {
-        
+        // Certains hébergement web bloque le header Authorization
+        // header.Authorization = `Bearer ${token}`
+
+        //Solution alternative : header personnalisé
+
+        headers['X-Auth-Token'] = token;
+        // Le JWT ne change pas
+        // Uniquement la façon de l'envoyer à l'API
+
+        // Authorization  header HTTP standard
+        // X-Auth-Token   header HTTP personnalisé
     }
+
+    const response = await fetch(`${API_URL}${endpoint}`, {
+        ...options,
+        headers,
+    });
+
+    const data = await response.json().catch(() => null);
+
+    if (!response.ok) {
+        // Si la réponse HTTP indique une erreur, lancer une exception avec le message d'erreur de l'API
+        throw new Error(data.message || "Une erreur est survenue.");
+    }
+
+    return data;
 
 }
